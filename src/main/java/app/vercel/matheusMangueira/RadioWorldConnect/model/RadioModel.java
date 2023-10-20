f82e1c869dbe696e2a3c5039989016246b7b0f31
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-20 16:32:26 -0300

    fix: add Radio class properties

--- src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/model/RadioModel.java
+++ src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/model/RadioModel.java
@@ -5,14 +5,16 @@ import java.util.UUID;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Entity(name = "tb_radio")
 @EqualsAndHashCode(of = "id")
 public class RadioModel {
@@ -20,5 +22,20 @@ public class RadioModel {
    @Id
    @GeneratedValue(generator = "UUID")
    private UUID id;
+
+   @NotBlank
+   private String name;
+
+   @NotBlank
+   private String urlRadio;
+
+   @NotBlank
+   private String image;
+
+   @NotNull
+   private Double longitude;
+
+   @NotNull
+   private Double latitude;
 
 }

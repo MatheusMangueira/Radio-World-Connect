6c5afcadaf99539769555c468c02741784de91f3
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-20 16:30:41 -0300

    feat: create IRadioRepository

--- src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/repository/IRadioRepository.java
+++ src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/repository/IRadioRepository.java
@@ -1,5 +1,18 @@
 package app.vercel.matheusMangueira.RadioWorldConnect.repository;
 
-public interface IRadioRepository {
-   
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import app.vercel.matheusMangueira.RadioWorldConnect.model.RadioModel;
+
+public interface IRadioRepository extends JpaRepository<RadioModel, UUID> {
+
+      boolean existsByName(String name);
+
+      @Query("SELECT r FROM tb_radio r WHERE r.name LIKE %:name%")
+      List<RadioModel> findByPartialName(@Param("name") String name);
 }

3e56fa9fcb6b1b65e323be9f10ddc3e6def759a0
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-20 16:30:17 -0300

    feat: create RadioService

--- src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/service/RadioService.java
+++ src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/service/RadioService.java
@@ -1,8 +1,49 @@
 package app.vercel.matheusMangueira.RadioWorldConnect.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import app.vercel.matheusMangueira.RadioWorldConnect.dtos.RadioDto;
+import app.vercel.matheusMangueira.RadioWorldConnect.dtos.mapper.RadioMapper;
+import app.vercel.matheusMangueira.RadioWorldConnect.model.RadioModel;
+import app.vercel.matheusMangueira.RadioWorldConnect.repository.IRadioRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class RadioService {
-   
+
+   private final IRadioRepository radioRepository;
+
+   public RadioService(IRadioRepository radioRepository) {
+      this.radioRepository = radioRepository;
+   }
+
+   @Transactional
+   public RadioModel save(RadioDto radioDto) {
+      validadeSave(radioDto);
+      return radioRepository.save(RadioMapper.toEntity(radioDto));
+   }
+
+   public List<RadioModel> findByPartialName(String name) {
+      List<RadioModel> all = radioRepository.findByPartialName(name);
+
+      if (all.isEmpty()) {
+         throw new RuntimeException("Radio not found");
+      }
+
+      return all;
+   }
+
+   public List<RadioModel> findAll() {
+      return radioRepository.findAll();
+   }
+
+   public void validadeSave(RadioDto radio) {
+      if (radioRepository.existsByName(
+            radio.getName()))
+
+         throw new RuntimeException("Radio already exists");
+
+   }
 }

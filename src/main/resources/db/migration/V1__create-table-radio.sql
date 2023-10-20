4b2a9ba213be18d71147d4245d857a1b38d91967
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-20 19:43:16 -0300

    fix: database and env configuration

---
+++ src/main/resources/db/migration/V1__create-table-radio.sql
@@ -0,0 +1,10 @@
+Create table radio(
+id bigint not null auto_increment,
+name varchar(100) not null,
+url_radio TEXT not null,
+image TEXT not null,
+longitude Double not null,
+latitude Double not null,
+
+primary key (id)
+)

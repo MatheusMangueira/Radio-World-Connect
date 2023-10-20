34f8a269f0c3172226872643b8641b7add1be71d
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-20 11:52:20 -0300

    feat: create layered architecture

---
+++ src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/RadioWorldConnectApplication.java
@@ -0,0 +1,13 @@
+package app.vercel.matheusMangueira.RadioWorldConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioWorldConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioWorldConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/vercel/matheusMangueira/RadioWorldConnect/RadioWorldConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vercel.matheusMangueira.RadioWorldConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioWorldConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

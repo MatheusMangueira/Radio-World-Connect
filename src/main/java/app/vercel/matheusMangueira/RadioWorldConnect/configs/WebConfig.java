7f56aa7dc9fe3f6f6ca5c6c44db187bcd10b3f62
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-21 02:03:46 -0300

    bugfix: problem Cors

--- src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/configs/WebConfig.java
+++ src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/configs/WebConfig.java
@@ -9,9 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-   public void AddCorsMappings(CorsRegistry registry) {
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
 
-      registry.addMapping("/**");
+      registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 
    }
 

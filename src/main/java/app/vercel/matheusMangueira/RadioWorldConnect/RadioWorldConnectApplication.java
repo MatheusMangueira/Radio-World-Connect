69422fdafbb2d5ff92d58447d3ea4b963f5db497
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-26 18:38:34 -0300

    feat: request control via IP

--- src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/RadioWorldConnectApplication.java
+++ src/main/java/app/vercel/matheusMangueira/RadioWorldConnect/RadioWorldConnectApplication.java
@@ -2,8 +2,10 @@ package app.vercel.matheusMangueira.RadioWorldConnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RadioWorldConnectApplication {
 
 	public static void main(String[] args) {
